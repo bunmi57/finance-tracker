3b09d85d6603ae12f8a19e9ba99a688332ecad88
Bunmi Akande <bunmiakande57@gmail.com>
2025-11-29 17:48:47 -0800

    Initial commit - set up Vite React client

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>Testing...</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
