0bdbd7b334af5dbba30bdbbf6b69b6b2edeba383
Bunmi Akande <bunmiakande57@gmail.com>
2025-12-02 20:08:37 -0800

    Add login route to server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended:true }));
 
 //Basic route 
 app.get("/", (req,res) =>{
     res.send("Server is running!");
 });
 
+//login route
+app.get("/login", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log("username:", username);
+    console.log("password:", password);
+    res.send("You are logged in!");
+});
 
 //Start server
 app.listen(port, () =>{
