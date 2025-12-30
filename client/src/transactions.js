597515ae2008431712efff747a6ba0a04e2edd9a
Bunmi Akande <bunmiakande57@gmail.com>
2025-12-30 08:45:12 +0100

    Create transactions array for testing

---
+++ client/src/transactions.js
@@ -0,0 +1,32 @@
+//For testing 
+const transactions = [
+        {
+            id: 1, 
+            user_id: 1, 
+            amount:400, 
+            type_transaction:"expense", 
+            category:"transportation",
+            date_transaction:"2025-03-11", 
+            note:"makeup items"
+        },
+
+        {
+            id: 2, 
+            user_id: 2, 
+            amount:300, 
+            type_transaction:"income", 
+            category:"income",
+            date_transaction:"2025-10-15", 
+            note:"Salary"
+        },
+
+        {
+            id: 3, 
+            user_id: 1, 
+            amount:200, 
+            type_transaction:"expense", 
+            category:"housing",
+            date_transaction:"2025-05-31", 
+            note:"Electricity"
+        }
+    ];
