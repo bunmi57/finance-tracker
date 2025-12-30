4fb2a8ae1e2d4e706f2f35b20d3dca401b6e8448
Bunmi Akande <bunmiakande57@gmail.com>
2025-12-30 08:39:25 +0100

    Update form input

--- client/src/components/FormInput.jsx
+++ client/src/components/FormInput.jsx
@@ -6,14 +6,12 @@ function FormInput(props){
  return(
     <>
         <div>
-            {/* Description  */}
-            <label>{props.title}</label>
-            <input 
-            onChange={props.handleChange}
-            type={props.type}
-            name={props.name} 
-            placeholder={props.placeholder}
-            value={props.value}/>
+                <input 
+                onChange={props.handleChange}
+                type={props.type}
+                name={props.name} 
+                placeholder={props.placeholder}
+                value={props.value}/>
         </div>  
     </>
  ) 
