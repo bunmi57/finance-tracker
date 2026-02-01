ec6dafe6421387774f4e93d122299d7b652d38d9
Bunmi Akande <bunmiakande57@gmail.com>
2026-02-01 12:16:37 -0800

    Add Income table, add input form

--- client/src/Pages/Income.jsx
+++ client/src/Pages/Income.jsx
@@ -15,216 +15,132 @@ To do List
 4. Add Read functionality 
 5. Add Update functionality 
 5. Add Delete functionality 
+*/
 
-
+/*
+Sunday 
+1. create table
+2. Add new income - frontend 
+3. Add new income - backend 
+Category: salary, freelance,bonus,investment,divident,gift,refund,other
 */
 
 function Income(){
-//Create state to store income
-const [income, setIncome] = useState([]);
+    //Create state to store income
+    const [income, setIncome] = useState([]);
 
-//Error state, stores backend validation or server error messages
-const [error, setError] = useState("");
+    //Error state, stores backend validation or server error messages
+    const [error, setError] = useState("");
 
-//State to store form input values for a single income
-const [item, setItem] = useState({
-    description: "",
-    category: "",
-    amount: "",
-    date:"",
-    note: ""
-});
-   
-
-/**************************************** Handle new income item ********************************************************** */
-
-function handleNewItemChange(event){
-    setError("");
-    const {name, value} = event.target;
-
-    setItem(prevItem => ({
-        ...prevItem,
-        [name]:value
-    }));
-}
-
-/**************************************** Handle change ********************************************************** */
-
-
-//updates state as the user types into the form inputs
-function handleChange(event){
-    setError("");
-    const {name, value} = event.target;
-
-    // Update the specific expense in the array
-    setIncome(prevIncome =>({
-            ...prevIncome,
-            [name]: value 
-         }));
-}
-return(
-    <>
-        <div className="income" >
-
-            {/* Table to show Income transaction */}
-            <form>
-                <table className="table">
-                    {/* Table heading */}
-                    <thead className = "thead-light">
-                        <tr>
-                            <th>For testing only</th> 
-                            <th>Income Sources</th>
-                            <th>Amount</th>
-                            <th>Note</th>
-                            <th>Date</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                         
-                        {incomes.map(inc =>
-                            <tr key={inc.id}>
-                                {/* for test only */}
-                                <td>{inc.id}</td> 
-
-                                <td>
-                                   <FormInput
-                                            title="Description"              // Label shown above the input
-                                            handleChange={handleChange()}  // pass the id       // Updates state when user types
-                                            type="text"                      // Text input
-                                            name="description"               // Matches item.description in state
-                                            placeholder="Enter description"  // Hint text inside input
-                                            value={inc.description}         // Controlled input value        
-                                    />
-                               
-                                </td>
-                                
-                                <td>
-                                    <FormInput
-                                            title="Category"
-                                            handleChange={handleChange()}  // pass the id       // Updates state when user types
-                                            type="text"
-                                            name="category"
-                                            placeholder="Enter category"
-                                            value={inc.category}         
-                                    />
-                                </td>
-
-                                <td>    
-                                    <FormInput
-                                            title="Amount"
-                                            handleChange={handleChange()}  // pass the id       // Updates state when user types
-                                            type="number"
-                                            name="amount"
-                                            placeholder="Enter number"
-                                            value={inc.amount}         
-                                    />
-                                </td>
-                                
-                                <td>
-                                    <FormInput
-                                            title="Note"
-                                            handleChange={handleChange()}  // pass the id       // Updates state when user types
-                                            type="text"
-                                            name="note"
-                                            placeholder="Enter note"
-                                            value={inc.note}         
-                                    />
-                                </td>
-                                <td>
-                                    <input 
-                                        onChange={handleChange()}                                      
-                                        type="date"
-                                        name="date_transaction"
-                                        value={formatDateForInput(inc.date_transaction)} //formatDateForInput is a function
-                                        min="2018-01-01"    //Earliest selectable date
-                                        max={getTodayDate()} //Prevents selecting future dates
-                                    />  
-                                </td>
-                            </tr>
-                        )}
-
-
-                        {/* Add input forms to enter a new income */}
-                        
-                        <tr>
-                            <td>               
-                                <FormInput
-                                    title="Description"              // Label shown above the input
-                                    handleChange={handleNewItemChange}       // Updates state when user types
-                                    type="text"                      // Text input
-                                    name="description"               // Matches item.description in state
-                                    placeholder="Enter description"  // Hint text inside input
-                                    value={item.description}         // Controlled input value        
-                                />
-                            </td>
-
-                            <td>                
-                                <FormInput
-                                    title="Category"
-                                    handleChange={handleNewItemChange}
-                                    type="text"
-                                    name="category"
-                                    placeholder="Enter category"
-                                    value={item.category}         
-                                />
-                            </td>
-
-                            <td>               
-                                <FormInput
-                                    title="Amount"
-                                    handleChange={handleNewItemChange}
-                                    type="number"
-                                    name="amount"
-                                    placeholder="Enter number"
-                                    value={item.amount}         
-                                />
-                            </td>
-
-                            <td>               
-                                <FormInput
-                                    title="Note"
-                                    handleChange={handleNewItemChange}
-                                    type="text"
-                                    name="note"
-                                    placeholder="Enter note"
-                                    value={item.note}         
-                                />
-                            </td>
-
-
-                            <td>               
-                                <input 
-                                    onChange={handleNewItemChange}
-                                    type="date"
-                                    name="date"
-                                    value={item.date}
-                                    min="2018-01-01"    //Earliest selectable date
-                                    max={getTodayDate} //Prevents selecting future dates
-                                />  
-                            </td>
-
-                            {/* error handling */}
-                        
-                            <td>
-                                {/* Show error message  */}
-                                {error && (<p style={{color: "red", marginBottom: "10px"}} > {error} </p>)}
-                                
-                                <button type="submit" > <SendIcon /> </button>
-                            </td>
-                        </tr>
+    //State to store form input values for a single income
+    const [item, setItem] = useState({
+        description: "",
+        source: "",
+        amount: "",
+        date:"",
+    });
     
 
-                    </tbody>
+    /**************************************** Handle new income item ********************************************************** */
 
-                </table>
-            </form>
+    function handleNewItemChange(event){
+        setError("");
+        //define name and value as entries from the input form 
+        const {name, value} = event.target;//name = event.target.name
+        //Now set the item  
 
-    
+        setItem(prevItem => ({
+            ...prevItem,
+            [name]:value //description: paycheck
+        }));
+    }
+
+    /**************************************** Handle change ********************************************************** */
+
+
+    //updates state as the user types into the form inputs
+    function handleChange(event){
+        console.log("name: ", event.target.name);
+        console.log("value: ", event.target.value);
+        // console.log("placeholder: ", event.target.placeholder);
+        // console.log("type: ", event.target.type);
+
+        const name = event.target.name; //e,g name = description, source
+        const value = event.target.value; //e,g name = description, source
+
+        //Update the item state as input changes
+        setItem(prevItem => ({
+            ...prevItem,
+            [name]:value})
+        );
+
+
+        // console.log("new entry");
+        // const {name, value} = event.target;
+
+        // setError("");
+
+        // // Update the specific expense in the array
+        // setIncome(prevIncome =>({
+        //         ...prevIncome,
+        //         [name]: value 
+        //     }));
+    }
+    return(
+        <>
+        <div className="expense">
+            <table className="table">
+                <tr>
+                    <th>Description</th>
+                    <th>Source</th>
+                    <th>Amount</th>
+                    <th>Date</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>First paycheck</td>
+                    <td>Salary</td>
+                    <td>5000</td>
+                    <td>2025-01-01</td>
+                    <td>Edit</td>
+                </tr>
+                <tr>
+                    <td>Freelance income</td>
+                    <td>Freelance</td>
+                    <td>1500</td>
+                    <td>2026-01-22</td>
+                    <td>Delete</td>
+                </tr>
+
+                {/* Add new form  */}
+                <tr>
+                    <td>
+                        <FormInput
+                            handleChange={handleChange}
+                            type="text"
+                            name="description" 
+                            placeholder="Enter description"
+                            value = {item.description} //use value that comes from state, hence keeping a single source of truth 
+                        />
+                    </td>
+                    <td>
+                        <FormInput
+                            handleChange={handleChange}
+                            type="number"
+                            name="amount"
+                            placeholder="Enter amount"
+                            value={item.amount}
+                        />
+                    </td>
+                    <td>2026-01-22</td>
+                    <td>Delete</td>
+                </tr>             
+
+
+            </table>
         </div>
-    </>
-    
-);
+        </>
+
+    )
 }
 export default Income;
